9e29996cc3e9ee80a083c471d3ffc8b743298d8c
tomomi <tomomi.nozue0127@gmail.com>
2018-10-12 02:23:16 +0900

    feat: Add new activity to input data

--- app/src/main/java/com/poc/android/myhospitals/MainActivity.java
+++ app/src/main/java/com/poc/android/myhospitals/MainActivity.java
@@ -3,22 +3,27 @@ package com.poc.android.myhospitals;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ItemViewModel mViewModel;
+    private ItemViewModel mItemViewModel;
+    public static final int NEW_WORD_ACTIVITY_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +40,10 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                Intent intent = new Intent(MainActivity.this, NewItemActivity.class);
+                startActivityForResult(intent, NEW_WORD_ACTIVITY_REQUEST_CODE);
+
             }
         });
     }
@@ -76,4 +85,16 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        
+        if (requestCode == NEW_WORD_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+            Item item = new Item(data.getStringExtra(NewItemActivity.EXTRA_REPLY));
+            mItemViewModel.insert(item);
+        } else {
+            Toast.makeText(this, "Item not saved because it's empty", Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
