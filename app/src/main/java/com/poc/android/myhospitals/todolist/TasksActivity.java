54817ae9a4e35511541f6d5936499d483e99e2f4
kimito <kimito@example.com>
2018-11-04 09:47:41 +0900

    feat: having a parent activity name specified

--- app/src/main/java/com/poc/android/myhospitals/todolist/TasksActivity.java
+++ app/src/main/java/com/poc/android/myhospitals/todolist/TasksActivity.java
@@ -4,17 +4,17 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.NavUtils;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.poc.android.myhospitals.R;
-
 import timber.log.Timber;
 
 public class TasksActivity extends AppCompatActivity {
@@ -31,6 +31,14 @@ public class TasksActivity extends AppCompatActivity {
         setupToolbar();
 
         setupNavigation();
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Timber.v("Tapped fab");
+            }
+        });
     }
 
     // add toolbar as action bar
@@ -77,5 +85,22 @@ public class TasksActivity extends AppCompatActivity {
                     }
             );
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
