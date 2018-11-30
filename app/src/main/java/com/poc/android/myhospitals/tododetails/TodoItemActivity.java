34f55172589d96016f4663236ceee4a4dca063b8
kimito <kimito@example.com>
2018-12-01 00:04:23 +0900

    feat: send item data and store in the realtime db

--- app/src/main/java/com/poc/android/myhospitals/tododetails/TodoItemActivity.java
+++ app/src/main/java/com/poc/android/myhospitals/tododetails/TodoItemActivity.java
@@ -3,13 +3,81 @@ package com.poc.android.myhospitals.tododetails;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.poc.android.myhospitals.R;
+import com.poc.android.myhospitals.todolist.TodoItem;
+
+import timber.log.Timber;
 
 public class TodoItemActivity extends AppCompatActivity {
+
+    public static final String ANONYMOUS = "anonymous";
+    public static final int DEFAULT_MSG_LENGTH_LIMIT = 1000;
+
+    private TextView mMessage;
+    private Button mButton;
+
+    private String mUserName;
+
+    // Firebase instance variables
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mMessagesDatabaseReference;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_item);
+
+        mUserName = ANONYMOUS;
+
+        // Initialize Firebase components
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mMessagesDatabaseReference = mFirebaseDatabase.getReference().child("messages");
+
+        mMessage = findViewById(R.id.itemTextView);
+        mButton = findViewById(R.id.sendButton);
+
+        //Enable send button when there's text to send
+        mMessage.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.toString().trim().length() > 0) {
+                    mButton.setEnabled(true);
+                } else {
+                    mButton.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        mMessage.setFilters(new InputFilter[]{new InputFilter.LengthFilter(DEFAULT_MSG_LENGTH_LIMIT)});
+
+        //Send button sends a item
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Send item on click
+                TodoItem todoItem = new TodoItem(mMessage.getText().toString(), mUserName);
+                mMessagesDatabaseReference.push().setValue(todoItem);
+
+                finish();
+            }
+        });
+
     }
 }
